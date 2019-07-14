15202744fc6db9029fa328c7f0e81da91c488446
cristianobrijanu <52889107+cristianobrijanu@users.noreply.github.com>
2019-07-14 19:16:14 +0300

    website upload

---
+++ precache-manifest.ed13af9da12808735e608f6ab04089c0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a25b7b5d5178fd9bf3f6ca366d03acb2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57376090aa3f367078ec",
+    "url": "/static/css/2.32bbf376.chunk.css"
+  },
+  {
+    "revision": "bd64518252ef2ad0caac",
+    "url": "/static/css/main.5b5b800a.chunk.css"
+  },
+  {
+    "revision": "57376090aa3f367078ec",
+    "url": "/static/js/2.85f0ef7b.chunk.js"
+  },
+  {
+    "revision": "bd64518252ef2ad0caac",
+    "url": "/static/js/main.457173fa.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ac51ad8a36d5bab6da11d454bcd06561",
+    "url": "/static/media/ProximaNova-Light_0.ac51ad8a.otf"
+  },
+  {
+    "revision": "bf9f5d50c1b928ff21436517a1a95ad9",
+    "url": "/static/media/ProximaNova-Regular_0.bf9f5d50.otf"
+  },
+  {
+    "revision": "f14eee643541cf03a10f26c944cc29f5",
+    "url": "/static/media/ProximaNova-Semibold_0.f14eee64.otf"
+  }
+]);
